28617edb3640f2ba21bb5864d5a1634d98aeab53
Ernesto Alejo <ernestokarim@gmail.com>
2022-09-15 10:02:05 +0200

    Export internal code from mpa.

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
+
 # vitemanifest
+
+[![Go Reference](https://pkg.go.dev/badge/github.com/altipla-consulting/vitemanifest.svg)](https://pkg.go.dev/github.com/altipla-consulting/vitemanifest)
+
 Read Vite manifest from Go.
+
+
+## Install
+
+```shell
+go get github.com/altipla-consulting/vitemanifest
+```
+
+
+## Usage
+
+Read 
---
+++ vite.go
@@ -0,0 +1,44 @@
+package vitemanifest
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+)
+
+// File stores the structure of the whole manifest file.
+type File map[string]Chunk
+
+// Chunk is every entry in the manifest file.
+type Chunk struct {
+	File           string
+	CSS            []string
+	Assets         []string
+	IsDynamicEntry bool
+	IsEntry        bool
+	DynamicImports []string
+}
+
+// Read the manifest file from its standard location of `dist/manifest.json`.
+func Read() (File, error) {
+	return ReadPath("dist/manifest.json")
+}
+
+// ReadPath reads the manifest file from a given path.
+func ReadPath(path string) (File, error) {
+	f, err := os.Open(path)
+	if err != nil {
+		if os.IsNotExist(err) {
+			return File{}, nil
+		}
+		return nil, fmt.Errorf("vitemanifest: cannot open manifest: %w", err)
+	}
+	defer f.Close()
+
+	manifest := make(File)
+	if err := json.NewDecoder(f).Decode(&manifest); err != nil {
+		return nil, fmt.Errorf("vitemanifest: cannot decode manifest: %w", err)
+	}
+
+	return manifest, nil
+}
